cdc0ddac21d4904c11ec12d8742f6abbde5d4b57
heilersk1 <heilersk1@udayton.edu>
2026-04-13 16:19:20 -0400

    Created navigation bar

--- store/urls.py
+++ store/urls.py
@@ -2,12 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), # homepage
+    path('', views.home, name='home'),
+
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+
     path('manager/', views.manager_dashboard, name='manager_dashboard'),
     path('employee/', views.employee_dashboard, name='employee_dashboard'),
-    path('store/', views.store_page, name='store'), # store page
-    path('events/', views.events_page, name='events'), # events page
+
+    path('store/', views.store_page, name='store_page'),
+    path('events/', views.events_page, name='events_page'),
+
     path('add-product/', views.add_product, name='add_product'),
     path('edit-product/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete-product/<int:product_id>/', views.delete_product, name='delete_product'),
